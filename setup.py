afcbc2bd376d0d19cc1cdcfdbcdd5369be00fadc
IonisFG <57777767+IonisFG@users.noreply.github.com>
2019-11-18 11:43:26 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,14 +12,6 @@ import subprocess
 import glob
 #import cython_gsl
 
-class ModelBuildingSetup(_build_py):
-    """ compile and build the scikit learn datasources that will be needed for 
-        prediction algorithms as part of the installation process """
-
-    def run(self):
-        print "building models..."
-        subprocess.check_call(['./scripts/_buildmodels', '--datadir', 'seqpred/data'])
-        _build_py.run(self)
 
 
 install_requires = [
@@ -39,50 +31,19 @@ test_requires = [
     ]
 
 
-setup(name='IPyBAT-ngstools',
+setup(name='HSModel',
       version='0.1',
       description='',
-      author='Chris Hart',
-      author_email='chart@isisph.com',
-      url='http://www.isisiph.com',
-      packages=['ngstools' ],
-      package_dir={'ngstools': 'ngstools'},
-      scripts= glob.glob('scripts/*'),
+      author='swag',
+      author_email='smukhopa@ionisph.com',
+      url='',
+      packages=['HSModel' ],
+      package_dir={'HSModel': 'HSModel'},
       install_requires=install_requires,
       tests_require=test_requires,
       test_suite="nose.collector",
-      ext_modules = cythonize("./ngstools/*.pyx"), 
+      ext_modules = cythonize("./HSModel/*.pyx"), 
       include_dirs=[numpy.get_include()],
       extra_compile_args=["-O3"]
      )
-
-# CHRIS: I want to include GSL and try to build extensions as follows, right now C++ compiler optimization is not used and gsl is disabled! 
-#setup(
-#  name = 'Test app',
-#  ext_modules=[
-#    Extension('test',
-#              sources=['test.pyx'],
-#              extra_compile_args=['-O3'],
-#              language='c++')
-#    ],
-#  cmdclass = {'build_ext': build_ext}
-#)
-
-# Setup examples 
-
-#extensions = [
-#    Extension("primes", ["primes.pyx"],
-#        include_dirs = [...],
-#        libraries = [...],
-#        library_dirs = [...]),
-#    # Everything but primes.pyx is included here.
-#    Extension("*", ["*.pyx"],
-#        include_dirs = [...],
-#        libraries = [...],
-#        library_dirs = [...]),
-#]
-#setup(
-#    name = "My hello app",
-#    ext_modules = cythonize(extensions),
-#)
 
